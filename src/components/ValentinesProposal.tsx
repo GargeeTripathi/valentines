1efa1bac2938895bc954c9577df3a2391318431d
Gargee Tripathi <gargee@example.com>
2026-02-14 02:10:40 +0530

    Added my photos and updated code

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -11,42 +11,24 @@ const playfairDisplay = Playfair_Display({
 
 // 36 images
 const images = [
-  "/game-photos/1.avif",
-  "/game-photos/2.avif",
-  "/game-photos/3.avif",
-  "/game-photos/4.avif",
-  "/game-photos/5.avif",
-  "/game-photos/6.avif",
-  "/game-photos/7.avif",
-  "/game-photos/8.avif",
-  "/game-photos/9.avif",
-  "/game-photos/10.avif",
-  "/game-photos/11.avif",
-  "/game-photos/12.avif",
-  "/game-photos/13.avif",
-  "/game-photos/14.avif",
-  "/game-photos/15.avif",
-  "/game-photos/16.avif",
-  "/game-photos/17.avif",
-  "/game-photos/18.avif",
-  "/game-photos/19.avif",
-  "/game-photos/20.avif",
-  "/game-photos/21.avif",
-  "/game-photos/22.avif",
-  "/game-photos/23.avif",
-  "/game-photos/24.avif",
-  "/game-photos/25.avif",
-  "/game-photos/26.avif",
-  "/game-photos/27.avif",
-  "/game-photos/28.avif",
-  "/game-photos/29.avif",
-  "/game-photos/30.avif",
-  "/game-photos/31.avif",
-  "/game-photos/32.avif",
-  "/game-photos/33.avif",
-  "/game-photos/34.avif",
-  "/game-photos/35.avif",
-  "/game-photos/36.avif",
+  "/game-photos/1.jpeg",
+  "/game-photos/2.jpeg",
+  "/game-photos/3.jpeg",
+  "/game-photos/4.jpeg",
+  "/game-photos/5.jpeg",
+  "/game-photos/6.jpeg",
+  "/game-photos/7.jpeg",
+  "/game-photos/8.jpeg",
+  "/game-photos/9.jpeg",
+  "/game-photos/10.jpeg",
+  "/game-photos/11.jpeg",
+  "/game-photos/12.jpeg",
+  "/game-photos/13.jpeg",
+  "/game-photos/14.jpeg",
+  "/game-photos/15.jpeg",
+  "/game-photos/16.jpeg",
+  "/game-photos/17.jpeg",
+  "/game-photos/18.jpeg"
 ];
 
 export default function ValentinesProposal() {
@@ -132,7 +114,7 @@ export default function ValentinesProposal() {
             <h2
               className={`text-5xl font-semibold mb-8 ${playfairDisplay.className}`}
             >
-              Will you be my Valentine?
+              Will you be my Valentine Krishi Baby and be mine forever?
             </h2>
             <Image
               src="/sad_hamster.png"
